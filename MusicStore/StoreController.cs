18fe6f0919be75cfa24733094aaa070669123f5e
jeroen <jeroen.smissaert@gmail.com>
2022-12-28 18:55:21 +0100

    Add project files.

---
+++ MusicStore/StoreController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicStore.ViewModels;
+
+namespace MusicStore
+{
+    public class StoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Create a list of genres
+            var genres = new List<string> { "Rock", "Jazz", "Country", "Pop", "Disco" };
+
+            // Create our view model
+            var viewModel = new StoreIndexVM { 
+                NumberOfGenres= genres.Count(),
+                Genres = genres };
+            return View();
+        }
+    }
+}
---
+++ MusicStore/ViewModels/StoreIndexVM.cs
@@ -0,0 +1,8 @@
+﻿namespace MusicStore.ViewModels
+{
+    public class StoreIndexVM
+    {
+        public int NumberOfGenres { get; set; }
+        public List<string> Genres { get; set;}
+    }
+}
